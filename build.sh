1f6a5ac86dee288a74eded560cb675eee81e06f8
Inderpreet <inderpreet.singh0511@gmail.com>
2026-04-26 16:06:17 +0530

    fix build requiremnets

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
 python manage.py migrate
+
+python manage.py shell <<EOF
+from food.models import FoodItem
+from django.core.management import call_command
+
+if FoodItem.objects.count() == 0:
+    call_command('loaddata', 'food_items.json')
+EOF
+
 python manage.py collectstatic --noinput
-python manage.py loaddata food_items.json
